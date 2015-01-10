fffed465f0f81dcc5f6cbfd02cffb4774aa645c2
musnit <musnit@gmail.com>
2015-01-10 12:51:29 +0200

    Add date, time, description to parse and event model

--- app/models/event.js
+++ app/models/event.js
@@ -2,6 +2,14 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  date: DS.attr('date'),
+  description: DS.attr('string'),
   userEvents: DS.hasMany('user-event'),
-  comments: DS.hasMany('comment')
+  comments: DS.hasMany('comment'),
+  shortDate: function(){
+    return this.get('date').toDateString();
+  }.property('date'),
+  time: function(){
+    return this.get('date').toTimeString().split(':').slice(0,2).join(':');
+  }.property('date')
 });
