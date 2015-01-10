a8336ba420e1a217067a5a9b2d3fb4c5f7e19a89
musnit <musnit@gmail.com>
2015-01-10 13:10:26 +0200

    Added place and address fields

--- app/models/event.js
+++ app/models/event.js
@@ -4,6 +4,10 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   date: DS.attr('date'),
   description: DS.attr('string'),
+  place: DS.attr('string'),
+  addressline1: DS.attr('string'),
+  addressline2: DS.attr('string'),
+  addressline3: DS.attr('string'),
   userEvents: DS.hasMany('user-event'),
   comments: DS.hasMany('comment'),
   shortDate: function(){
