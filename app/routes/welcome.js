a195932854499bec02035797fb4488293da6d5ff
musnit <musnit@gmail.com>
2015-01-10 13:13:08 +0200

    Refactor using inheritance

--- app/routes/welcome.js
+++ app/routes/welcome.js
@@ -1,34 +1,6 @@
 import Ember from 'ember';
+import EventsRoute from 'eventsapp/routes/events';
 
-export default Ember.Route.extend({
+export default EventsRoute.extend({
   controllerName: 'events',
-  model: function(){
-    return this.get('store').find('event');
-  },
-  afterModel: function(resolvedModel, transition, queryParams){
-    if (this.get('session.content.user') !== undefined){
-      return this.get('store').find('userEvent', {
-        where: {
-          parseUser: {
-            "__type":  "Pointer",
-            "className": "_User",
-            "objectId": this.get('session.user.id')
-          }
-        }
-      });
-    }
-  },
-  setupController: function(controller, model){
-    controller.set('model', model);
-    if (this.get('session.content.user') !== undefined){
-      controller.resetEventGroups();
-    }
-    controller.setTopUpcomingEvents();
-  },
-  actions:{
-    saveUserEvent: function(userEvent){
-      userEvent.save();
-      this.controller.resetEventGroups();
-    }
-  }
 });
